e8ada1e2524fcb2c717368e2c85215966230ebbc
satishmzeemart <satishm@zeemart.asia>
2021-02-12 16:15:56 +0530

    login page added

---
+++ lib/utils/color.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+/**
+ * Created by RajPrudhviMarella on 11/Feb/2021.
+ */
+
+Color faintGrey = Color.fromRGBO(244, 244, 244, 1);
+Color greyText = Color.fromRGBO(153, 153, 153, 1);
+Color buttonBlue = Color(0xff115ece);
+Color lightGreen = Color(0xff01ce5f);
---
+++ lib/utils/constants.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+
+/**
+ * Created by RajPrudhviMarella on 11/Feb/2021.
+ */
+
+class Constants {
+  // Strings
+  static const String app_name = "Zeemart Supplier";
+  static const String txt_change_password = "Change Password";
+  static const String txt_help = "Help";
+  static const String txt_ask_zeemart = "Ask Zeemart";
+  static const String txt_send_feed_back = "Send FeedBack";
+  static const String txt_terms_of_use = "Terms Of Use";
+  static const String txt_privacy_policy = "Privacy policy";
+  static const String txt_log_out = "Sign Out";
+  static const String txt_support = "Support";
+  static const String txt_account = "Account";
+
+  static const String loginInfo = "loginInfo";
+}
+
+class SharedPref {
+  saveData(String key, value) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString(key, json.encode(value));
+  }
+
+  readData(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    return json.decode(prefs.getString(key));
+  }
+}
